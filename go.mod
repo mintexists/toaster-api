9a563d1bac569b778008554ce174a515081ebf99
evaexists <bates.nevin@doversherborn.org>
2022-03-25 10:05:54 -0400

    Rewrite the API to have better routes and have better performance.

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module toaster-api
 go 1.16
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/joho/godotenv v1.4.0
-	github.com/julienschmidt/httprouter v1.3.0
 )
--- main.go
+++ main.go
@@ -12,47 +12,167 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
-	"strings"
 	"text/template"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/joho/godotenv"
-	"github.com/julienschmidt/httprouter"
 )
 
-var noCacheHeaders = map[string]string{
-	"Expires":         "0",
-	"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
-	"Pragma":          "no-cache",
-	"X-Accel-Expires": "0",
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	paths, max := loadPaths()
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Use(middleware.RedirectSlashes)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		type Route struct {
+			Route       string `json:"route"`
+			Method      string `json:"method"`
+			Description string `json:"description"`
+		}
+		type Api struct {
+			Info   string  `json:"info"`
+			Routes []Route `json:"routes"`
+			Max    int     `json:"max"`
+		}
+		api := Api{
+			Info: "Toaster Image API",
+			Routes: []Route{
+				{
+					Route:       "/",
+					Method:      "GET",
+					Description: "Get the API info",
+				},
+				{
+					Route:       "/{id}",
+					Method:      "GET",
+					Description: "Get the image by id",
+				},
+				{
+					Route:       "/random",
+					Method:      "GET",
+					Description: "Get a random image",
+				},
+				{
+					Route:       "/embed/{id}",
+					Method:      "GET",
+					Description: "Get the image as an opengraph/twitter embed card",
+				},
+				{
+					Route:       "/embed/random",
+					Method:      "GET",
+					Description: "Get a random image as an opengraph/twitter embed card",
+				},
+			},
+			Max: max - 1,
+		}
+		json.NewEncoder(w).Encode(api)
+	})
+
+	r.Get("/{img:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		number, _ := strconv.Atoi(chi.URLParam(r, "img"))
+		number = number % max
+
+		fileBytes, err := ioutil.ReadFile(paths[number])
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(fileBytes)
+	})
+
+	r.Get("/embed/{img:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("embed.html")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		number, _ := strconv.Atoi(chi.URLParam(r, "img"))
+		number = number % max
+
+		url := fmt.Sprintf("http://%s/%d", r.Host, number)
+
+		type data struct {
+			ID  int
+			URL string
+		}
+
+		d := data{
+			ID:  number,
+			URL: url,
+		}
+
+		tmpl.Execute(w, d)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.NoCache)
+
+		r.Get("/random", func(w http.ResponseWriter, r *http.Request) {
+			number := rand.Intn(len(paths)) % max
+
+			fileBytes, err := ioutil.ReadFile(paths[number])
+			if err != nil {
+				panic(err)
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(fileBytes)
+		})
+
+		r.Get("/embed/random", func(w http.ResponseWriter, r *http.Request) {
+			tmpl, err := template.ParseFiles("embed.html")
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			number := rand.Intn(len(paths)) % max
+
+			url := fmt.Sprintf("http://%s/%d", r.Host, number)
+
+			type data struct {
+				ID  int
+				URL string
+			}
+
+			d := data{
+				ID:  number,
+				URL: url,
+			}
+
+			tmpl.Execute(w, d)
+		})
+	})
+
+	http.ListenAndServe(os.Getenv("PORT"), r)
 }
 
-func imageHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func loadPaths() ([]string, int) {
+	paths, err := os.ReadDir("./Toasters/Toasters/")
+	if err != nil {
+		panic(err)
+	}
 
 	regex, err := regexp.Compile("[0-9]+")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	paths, err := os.ReadDir("./Toasters/Toasters/")
-	if err != nil {
-		panic(err)
-	}
-
-	number, err := strconv.Atoi(ps.ByName("id"))
-	if err != nil {
-		if strings.Split(ps.ByName("id"), "/")[0] == "random" {
-			for k, v := range noCacheHeaders {
-				w.Header().Set(k, v)
-			}
-			number = rand.Intn(len(paths) - 1)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "404 Not Found")
-			return
-		}
-	}
-
-	number = number % len(paths)
+	max := len(paths)
 
 	sort.Slice(paths, func(i, j int) bool {
 		a, err := strconv.Atoi(regex.FindString(paths[i].Name()))
@@ -66,118 +186,11 @@ func imageHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) 
 		return a < b
 	})
 
-	go addStats()
-	http.ServeFile(w, r, filepath.Join("./Toasters/Toasters/", paths[number].Name()))
-}
+	pathString := make([]string, max)
 
-type EmbedData struct {
-	PagePath string
-	ID       int
-	URL      string
-}
-
-func embedImage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Println(r.URL.Scheme)
-	tmpl, err := template.ParseFiles("embed.html")
-	if err != nil {
-		log.Println(err)
-		return
+	for i, path := range paths {
+		pathString[i] = filepath.Join("./Toasters/Toasters/", path.Name())
 	}
 
-	paths, err := os.ReadDir("./Toasters/Toasters/")
-	if err != nil {
-		panic(err)
-	}
-
-	number, err := strconv.Atoi(ps.ByName("id"))
-	if err != nil {
-		if strings.Split(ps.ByName("id"), "/")[0] == "random" {
-			for k, v := range noCacheHeaders {
-				w.Header().Set(k, v)
-			}
-			number = rand.Intn(len(paths) - 1)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "404 Not Found")
-			return
-		}
-	}
-
-	number = number % len(paths)
-	pagePath := fmt.Sprintf("http://%s/img/%d", r.Host, number)
-
-	page := EmbedData{
-		PagePath: pagePath,
-		ID:       number,
-		URL:      pagePath,
-	}
-
-	go addStats()
-	tmpl.Execute(w, page)
-}
-
-func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	paths, err := filepath.Glob("./Toasters/Toasters/*")
-	if err != nil {
-		panic(err)
-	}
-
-	stats := getStats()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "{\"info\": \"Toaster Image API. Routes are /img/[number], /img/random, /pic/[number], /pic/random, /embed/[number], /img/random. Add /[anything] to the end of the URL to avoid cache issues\", \"max\": %d, \"hits\": %d}", len(paths)-1, stats.Hits)
-	go addStats()
-}
-
-func getStats() Stats {
-	jsonFile, err := os.Open("stats.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var stats Stats
-	json.Unmarshal(byteValue, &stats)
-	return stats
-}
-
-func addStats() {
-	jsonFile, err := os.Open("stats.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var stats Stats
-	json.Unmarshal(byteValue, &stats)
-	stats.Hits++
-	file, _ := json.MarshalIndent(stats, "", " ")
-	ioutil.WriteFile("stats.json", file, 0644)
-}
-
-type Stats struct {
-	Hits int `json:"hits"`
-}
-
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
-
-	router := httprouter.New()
-	router.GET("/embed/:id", embedImage)
-	router.GET("/embed/:id/*_", embedImage)
-	router.GET("/img/:id/*_", imageHandler)
-	router.GET("/img/:id", imageHandler)
-	router.GET("/pic/:id/*_", imageHandler)
-	router.GET("/pic/:id", imageHandler)
-	router.GET("/", api)
-	fmt.Println("Listening on port", os.Getenv("PORT"))
-	http.ListenAndServe(os.Getenv("PORT"), router)
+	return pathString, max
 }
